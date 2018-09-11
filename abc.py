32794566ac4aeb17acd30af453b850ef42b229b9
tanushr <43023087+tanushr@users.noreply.github.com>
2018-09-11 12:26:08 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value for a: "))
+b=int(input("enter the value for b: "))
+if(a>b):
+   print(a,"is greater than",b)
+elif(a==b):
+   print(a,"is equal to",b)   
+else:
+   print(b,"is greater than",a) 
